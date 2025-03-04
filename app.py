4da11f72d6bbefc2c5d980096668264ac8a6d416
Nasir_Mehmood <nasirjani757571@gmail.com>
2025-03-04 09:05:08 +0500

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def get_instagram_links():
     try:
         conn = psycopg2.connect(DATABASE_URL)
         cursor = conn.cursor()
-        cursor.execute('SELECT page_name, link, timestamp FROM instagram_links')
+        cursor.execute('SELECT page_name, link, timestamp FROM instagram_links ORDER BY timestamp DESC')
         data = cursor.fetchall()
 
         # Convert tuples into dictionaries using list comprehension
